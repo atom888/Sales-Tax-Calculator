78459eb3e561ad02796c88956b016efd31ca1136
Adam Dipinto <adam.dipinto@gmail.com>
2016-11-03 02:11:41 +0000

    Adding first version to sales tax calculator assignment - need to correct function names

---
+++ sales-tax.js
@@ -0,0 +1,108 @@
+//Lighthouse Labs - W1D3 Exercise - Sales Tax Calculator //
+
+var salesTaxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
+
+var companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
+  }
+];
+
+
+function calcSalesTotals (data) {
+  var answer = [];
+  for (var i = 0; i < data.length; i += 1) {
+    var companyInProv = data[i];
+    var partialAnswer = {
+      name: companyInProv.name,
+      province: companyInProv.province
+    };
+    partialAnswer.totalSales = companyInProv.sales.reduce(function(runTotal, currTotal) {
+      return runTotal + currTotal;
+    }, 0);
+    answer.push(partialAnswer);
+   // console.log(answer);
+  }
+  return answer;
+}
+
+var newData = calcSalesTotals(companySalesData);
+
+//console.log(newData);
+
+function calcSalesTax(salesData, rates) {
+  for (var i = 0; i < salesData.length; i += 1) {
+    var province = salesData[i].province;
+    var taxRate = rates[province];
+    var totalSales = salesData[i].totalSales;
+    var totalTax = taxRate * totalSales;
+    // Store the result into a new key
+    salesData[i].totalTax = totalTax;
+  }
+  return salesData;
+}
+
+var newData2 = calcSalesTax(newData, salesTaxRates);
+
+console.log(newData2);
+
+function companyAdd(salesData) {
+  var company = {};
+  for (var i = 0; i < salesData.length; i+= 1) {
+
+    name = salesData[i].name;
+    totalSalesValue = salesData[i].totalSales;
+    totalTaxesValue = salesData[i].totalTax;
+
+    if (!company[name]) {
+      company[name] = {
+        totalSales: totalSalesValue,
+        totalTaxes: totalTaxesValue
+      }
+    } else {
+      company[name].totalSales += salesData[i].totalSales;
+      company[name].totalTaxes += salesData[i].totalTax;
+    }
+  }
+  return company;
+}
+
+
+
+
+
+
+var newData3 = companyAdd(newData2);
+
+console.log(newData3);
+
+//var results = salesTaxReport(companySalesData, salesTaxRates);
+
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
